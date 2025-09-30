2e0b18ac9f9d3a9f80a5a8f1b2acaed0473dfba1
ivanderaryasuta-dot <ivanderaryasuta@gmail.com>
2025-09-30 09:45:05 +0700

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,11 @@
 public class test{
     public static void main(String[] args) {
         System.out.println("hALO INI USER 1");
-
+          
+    }
+}
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hi ini baris kode pertama");
     }
 }
