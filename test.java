6eff11f758371bfc8b2880b27b7ece89186dc253
aryavan32 <ivanderaryasuta@gmail.com>
2025-09-30 10:03:41 +0700

    modify test

--- test.java
+++ test.java
@@ -1,11 +1,8 @@
 public class test{
     public static void main(String[] args) {
         System.out.println("hALO INI USER 1");
-          
-    }
-}
-public class test {
-    public static void main(String[] args) {
+        
         System.out.println("Hi ini baris kode pertama");
+        System.out.println("HALO INI USER 2");
     }
 }
